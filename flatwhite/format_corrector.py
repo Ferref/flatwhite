366f6213ca110a2bec069cf344d455c0fb18eb90
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-07-22 16:48:36 +0200

    Added cleaning option to validation functions and update correct_name.

--- flatwhite/format_corrector.py
+++
@@ -1,7 +0,0 @@
-def correct_name(name: str) -> str:
-    name = name.strip()
-    name_parts = name.split()
-
-    capitalize_parts = ' '.join(part.capitalize() for part in name_parts)
-
-    return capitalize_parts
--- flatwhite/format_validator.py
+++ flatwhite/format_validator.py
@@ -1,41 +1,49 @@
-# flatwhite/format_validator.py
-import re 
+import re
 
-def validate_email(email) -> bool:
-
+def validate_email(email, cleaning=False):
     valid_email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
     if re.fullmatch(valid_email_pattern, email):
         return True
-    else:
-        return False
-    
-def validate_phone(phone) -> bool:
+    elif cleaning:
+        email = email.strip().lower()
+        if re.fullmatch(valid_email_pattern, email):
+            return email
+    return False
 
+def validate_phone(phone, cleaning=False):
     extract_numbers = r'\d+'
-
     numbers_from_phone = re.findall(extract_numbers, phone)
     numbers_from_phone = ''.join(numbers_from_phone)
-
+    
     correct_phone_number_length = 10
 
     if len(numbers_from_phone) == correct_phone_number_length:
         return True
-    else:
-        return False
-        
+    elif cleaning:
+        phone = ''.join(numbers_from_phone)
+        if len(phone) == correct_phone_number_length:
+            return phone
+    return False
 
+def validate_age(age, cleaning=False):
+    extract_numbers = r'\d+'
+    numbers_from_age = re.findall(extract_numbers, age)
+    age = ''.join(numbers_from_age)
 
+    if age.isdigit():
+        return True
+    elif cleaning:
+        if age.isdigit():
+            return age
+    return False
 
-
-
-
-
-
+def correct_name(name, cleaning=False):
+    name = name.strip()
+    name_parts = name.split()
     
+    capitalize_parts = ' '.join(part.capitalize() for part in name_parts)
 
-
-
-
-
-
+    if cleaning:
+        return capitalize_parts
+    return capitalize_parts
