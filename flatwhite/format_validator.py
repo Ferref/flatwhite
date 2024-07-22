4d3cff57948865b86420126b8cbfb3a97a326e14
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-07-22 16:29:38 +0200

    added validate_phone(phone)

--- flatwhite/format_validator.py
+++ flatwhite/format_validator.py
@@ -2,14 +2,36 @@
 import re 
 
 def validate_email(email):
-    valid_email = False
+    valid_email_status = False
 
-    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+    valid_email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
 
-    if re.fullmatch(regex, email):
-        valid_email = True
-        return valid_email
+    if re.fullmatch(valid_email_pattern, email):
+        valid_email_status = True
+        return valid_email_status
     
+def validate_phone(phone):
+    validate_phone_status = False
+
+    extract_numbers = r'\d+'
+
+    numbers_from_phone = re.findall(extract_numbers, phone)
+    numbers_from_phone = ''.join(numbers_from_phone)
+
+    correct_phone_number_length = 10
+
+    if len(numbers_from_phone) == correct_phone_number_length:
+        validate_phone_status = True
+        
+    return validate_phone_status
+
+
+
+
+
+
+    
+
 
 
 
