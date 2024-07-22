57d3804db5c9e8879790e2dcf861bb4d0f24505b
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-07-22 17:07:34 +0200

    file_handler.py added functions

--- flatwhite/file_handler.py
+++ flatwhite/file_handler.py
@@ -1,5 +1,19 @@
 # flatwhite/file_handler.py
+import os, shutil
 
-def handle_file(file_path):
-    # Add your file handling logic here
-    pass
+def get_file_names(file_path):
+    os.listdir(file_path)
+
+def path_exists(file_path, if_not_exists_create=False):
+
+    exists = os.path.exists(file_path)
+
+    if not exists and if_not_exists_create:
+        with open(file_path, 'w') as f:
+            pass
+        return True
+
+    return exists
+    
+
+
