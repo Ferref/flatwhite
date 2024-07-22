2f707dee22979cc15deb28ddf9f444cd1008b775
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-07-22 15:14:45 +0200

    basics

---
+++ flatwhite/format_validator.py
@@ -0,0 +1,5 @@
+# flatwhite/format_validator.py
+
+def validate_format(data):
+    # Add your validation logic here
+    pass
---
+++ setup.py
@@ -0,0 +1,24 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='flatwhite',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # If you have command-line scripts, list them here
+        ],
+    },
+    url='https://github.com/Ferref/flatwhite',
+    license='MIT',
+    author='Ferenc Kovács',
+    author_email='kovacsferenc026@gmail.com',
+    description='A package for validating formats and handling files.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+)
