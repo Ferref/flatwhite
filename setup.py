d31c7b300aea963e5c6823267f52fa4e43963ca9
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-08-05 13:54:28 +0200

    increased the version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='flatwhite',
-    version='0.3.0',
+    version='0.3.1',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
