ebab360fbed431dce9092ff0a89a89a0e5cf6c92
Ferenc Kovács <kovacsferenc026@gmail.com>
2024-08-06 13:50:15 +0200

    def list_directory_structure(root_dir, indent_level)

--- flatwhite/file_handler.py
+++ flatwhite/file_handler.py
@@ -177,7 +177,28 @@ def generate_folders(base_path, folders):
         if isinstance(subfolder_structure, dict):
             generate_folders(folder_path, subfolder_structure)
 
-# Example usage:
-# organize_files('/path/to/directory', pics=True, sounds=True, docs=True)
-# delete_files('/path/to/directory', pics=True, additional_extensions='.tmp,.log')
-# generate_folders('/base/path', {'books': {'fantasy': {}, 'crime': {}}, 'audiobooks': {}})
+
+def list_directory_structure(root_dir, indent_level=0):
+    """
+    Recursively lists the directory structure starting from the root_dir.
+
+    Parameters:
+    root_dir (str): The root directory to start listing from.
+    indent_level (int): The current level of indentation for pretty printing.
+    """
+    try:
+        # List all files and directories in the current directory
+        items = os.listdir(root_dir)
+    except PermissionError:
+        # Skip directories that we don't have permission to access
+        return
+
+    for item in items:
+        item_path = os.path.join(root_dir, item)
+        print(' ' * indent_level + '|-- ' + item)
+
+        # If the item is a directory, recursively list its contents
+        if os.path.isdir(item_path):
+            list_directory_structure(item_path, indent_level + 4)
+
+
